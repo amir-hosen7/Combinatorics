ba67370b3801e0b769697bb3d7af541132c1660f
Amantu Amir <42311923+Amantu-Amir@users.noreply.github.com>
2021-02-12 01:04:54 +0600

    Create SPOJ: MARBLES - Marbles.cpp

---
+++ Stars and Bars/SPOJ: MARBLES - Marbles.cpp
@@ -0,0 +1,60 @@
+
+//https://www.spoj.com/problems/MARBLES/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define prnt(a) cout<<a
+#define sp cout<<" "
+#define nl cout<<"\n"
+#define in1(a) cin>>a
+#define in2(a,b) cin>>a>>b
+#define in3(a,b,c) cin>>a>>b>>c;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define rep(i,a,b) for(ll i=a; i<b; i++) 
+typedef tree < ll, null_type, less < ll >, 
+rb_tree_tag, tree_order_statistics_node_update > ordered_set;
+string toString(ll n){stringstream ss; ss<<n; return ss.str();}
+void yesNo(bool ck){cout<<(ck? "YES\n":"NO\n");}
+vector<ll> removeDup(vector<ll>v){
+sort(all(v)); v.resize(distance(v.begin(),unique(all(v))));return v;}
+
+//------------------------------------------------------------------------------//
+const int N=1e6;
+
+
+ll nCr(ll n,ll r){
+	ll res=1;
+	if(r>(n-r)){
+		r=n-r;
+	}
+	rep(i,0,r){
+		res*=(n-i);
+		res/=(i+1);
+	}
+	return res;
+}
+
+int main(){
+
+	//freopen("output.txt", "w", stdout);
+	//freopen("input.txt", "r", stdin);
+	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	ll t=1; cin>>t;
+	rep(T,1,t+1){
+		ll n,k; cin>>n>>k;
+		ll bars=k-1;
+		ll stars=n+bars-k;
+		cout<<nCr(stars,bars)<<"\n";
+	}
+	return 0;
+}
+
